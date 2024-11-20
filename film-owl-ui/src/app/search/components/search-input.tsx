032e710dbeebb419c3361673cbcb72bb82de2a1c
Felo Tadros <felotadros326@gmail.com>
2024-11-19 22:57:57 -0500

    fix page loading with right data model

--- film-owl-ui/src/app/search/components/search-input.tsx
+++ film-owl-ui/src/app/search/components/search-input.tsx
@@ -1,47 +1,46 @@
-"use client";
-import { useRouter } from "next/navigation";
+// "use client";
 import { Input } from "@nextui-org/react";
-import React, { useState } from "react";
-// import {SearchIcon} from "./SearchIcon.jsx";
 import { SearchCodeIcon } from "lucide-react";
-import SearchPage from "../page";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const SearchInput: React.FC = () => {
-  const [query, setQuery] = useState("");
-  const router = useRouter();
+	const [query, setQuery] = useState("");
+	const router = useRouter();
 
-  const handleSearch = () => {
-    if (!query.trim()) return;
+	const handleSearch = () => {
+		const trimmedQuery = query.trim();
+		if (trimmedQuery) {
+			router.push(`/search?query=${encodeURIComponent(trimmedQuery)}`);
+		}
+	};
 
-    router.push(`/search?query=${encodeURIComponent(query)}`);
-  };
+	const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+		if (event.key === "Enter") {
+			handleSearch();
+		}
+	};
 
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      handleSearch();
-    }
-  };
-
-  return (
-    <div className="flex items-center border border-gray-300 rounded-md p-2">
-      <SearchCodeIcon className="text-black/50 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
-      <Input
-        placeholder="Type a movie name here..."
-        type="search"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyDown={handleKeyPress}
-        className="ml-2 flex-grow bg-transparent outline-none dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-teal-500 dark:focus:ring-teal-300"      
-        style={{ height: '100%' }}
-      />
-      <button
-        onClick={handleSearch}
-        className= "bg-teal-600 hover:bg-teal-700 text-white font-bold px-4 py-3 rounded-r-md focus:outline-none focus:ring-2 focus:ring-teal-500 dark:focus:ring-teal-300"
-      >
-        Search
-      </button>
-    </div>
-  );
+	return (
+		<div className="flex items-center border border-gray-300 rounded-md p-2">
+			<SearchCodeIcon className="text-black/50 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+			<Input
+				placeholder="Type a movie name here..."
+				type="search"
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+				onKeyDown={handleKeyPress}
+				className="ml-2 flex-grow bg-transparent outline-none dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-teal-500 dark:focus:ring-teal-300"
+				style={{ height: "100%" }}
+			/>
+			<button
+				onClick={handleSearch}
+				className="bg-teal-600 hover:bg-teal-700 text-white font-bold px-4 py-3 rounded-r-md focus:outline-none focus:ring-2 focus:ring-teal-500 dark:focus:ring-teal-300"
+			>
+				Search
+			</button>
+		</div>
+	);
 };
 
 export default SearchInput;
--- film-owl-ui/src/app/search/page.tsx
+++ film-owl-ui/src/app/search/page.tsx
@@ -1,54 +1,59 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { fetchMovies } from "@/lib/api";
-import SearchInput from "./components/search-input";
-// import { Movie } from "@/lib/api";
+import React, { useEffect, useState } from "react";
+
 interface Movie {
-  image_url: string;
-  genre: string;
-  id: string;
-  release_date: string;
-  title: string;
+	image_url: string;
+	genre: string;
+	id: string;
+	release_date: string;
+	title: string;
+	created_at: string;
+	imdbID: string;
 }
-
 const SearchPage: React.FC = () => {
+	const searchParams = useSearchParams();
+	const query = searchParams.get("query") || "";
+	const [results, setResults] = useState<Movie[]>([]);
 
-  const searchParams = useSearchParams();
-  const query = searchParams.get("query") || ""; // Get the query parameter from the URL
-  const [results, setResults] = useState([]);
+	useEffect(() => {
+		if (!query) return;
 
-  useEffect(() => {
-    if (!query) return;
+		// Fetch data from the backend
+		fetch(`http://localhost:5002/search/${encodeURIComponent(query)}`)
+			.then((response) => {
+				if (!response.ok) throw new Error("Failed to fetch movies");
+				return response.json();
+			})
+			.then((data) => {
+				if (!query) return;
+				setResults(data);
+			})
+			.catch((error) => console.error("Error fetching movies:", error));
+	}, [query]);
 
-    // Fetch data from the backend
-    fetch(`http://localhost:5002/search/${encodeURIComponent(query)}`)
-      .then((response) => {
-        if (!response.ok) throw new Error("Failed to fetch movies");
-        return response.json();
-      })
-      .then((data) => setResults(data))
-      .catch((error) => console.error("Error fetching movies:", error));
-  }, [query]);
-
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold mb-8">Results for "{query}"</h1>
-      {results.map((movie) => (
-          <li key={movie.imdbID} className="mb-4">
-            <img
-              src={movie.image_url !== "N/A" ? movie.image_url : "/placeholder.png"}
-              alt={movie.title}
-              className="w-full h-64 object-cover mb-2 rounded"
-            />
-            <h2 className="text-lg font-bold">{movie.title}</h2>
-            <p>{movie.created_at}</p>
-          </li>
-        ))}
-     
-    </div>
-  );
+	return (
+		<div className="container mx-auto px-4 py-8">
+			<h1 className="text-4xl font-bold mb-8">Results for "{query}"</h1>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+				{results.map((movie) => (
+					<div key={movie.id} className="mb-4">
+						<img
+							src={
+								movie.image_url !== "N/A"
+									? movie.image_url
+									: "/placeholder.png"
+							}
+							alt={movie.title}
+							className="w-full h-447 object-cover mb-2 rounded"
+						/>
+						<h2 className="text-lg font-bold">{movie.title}</h2>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default SearchPage;
