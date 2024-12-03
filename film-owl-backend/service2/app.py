c5ff09ba872ed96e242229fca82646363e877a5c
Maryan <maryanay2000@gmail.com>
2024-12-03 17:09:36 -0500

    Debugging login features

--- film-owl-backend/service2/app.py
+++ film-owl-backend/service2/app.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI, HTTPException
+# import sys
+# import os
+
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# from service1.app import Movie
+
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional, List
 from sqlalchemy import Integer
@@ -40,15 +46,27 @@ def get_all_users():
 
 @app.post("/users")
 def create_user(user: User):
+    print(f"Received signup request: {user}")
     engine = create_engine("postgresql://postgres:postgres@db/film-owl")
     with Session(engine) as session:
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-        return user
+      existing_user = session.exec(
+      select(User).where((User.email == user.email) | (User.username == user.username))
+       ).first()
+      if existing_user:
+        raise HTTPException(status_code=400, detail="Email or username already exists")
+        
+      if not user.password:
+        raise HTTPException(status_code=400, detail="Password cannot be empty")
+      
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+    # return {"message": "User successfully created", "username": user.username,}
 
 @app.get("/users/{user_id}")
 def get_user_by_id(user_id: int):
+    
     engine = create_engine("postgresql://postgres:postgres@db/film-owl")
     with Session(engine) as session:
         statement = select(User).where(User.id == user_id)
@@ -93,7 +111,53 @@ def login(request: LoginRequest):
         if(request.password != user.password):
             raise HTTPException(status_code=400, detail="Invalid email or password")
         
-        return {"message": "Login successful", "user": user.id}
+        return {"message": "Login successful", "user": user.username}
+        # return {"message": "Login successful", "user": {"username": request.username, "email": user.email, "id": user.id}}
+
+# def get_db():
+#     engine = create_engine("postgresql://postgres:postgres@db/film-owl")
+#     with Session(engine()) as session:
+#         yield session
+
+# @app.get("/favorites/{user_id}")
+# def get_user_favourites(user_id: int, db: Session = Depends(get_db)):
+#     user = db.query(User).filter(User.id == user_id).first()
+#     if not user:
+#         raise HTTPException(status_code=404, detail="User not found")
+
+#     movie_ids = user.favorites
+#     movies = db.query(Movie).filter(Movie.id.in_(movie_ids)).all()
+
+#     return {"favourites": movies}
+
+# @app.post("/add_favourite/{user_id}/{movie_id}")
+# def add_favourite(user_id: int, movie_id: int, db: Session = Depends(get_db)):
+#     user = db.query(User).filter(User.id == user_id).first()
+#     if not user:
+#         raise HTTPException(status_code=404, detail="User not found")
+    
+#     if movie_id in user.favorites:
+#         raise HTTPException(status_code=400, detail="Movie already in favourites")
+#     user.favorites.append(movie_id)
+#     db.commit()
+#     db.refresh(user)
+
+#     return {"message": "Movie added to favourites", "favourites": user.favorites}
+
+# @app.delete("/remove_favourite/{user_id}/{movie_id}")
+# def remove_favourite(user_id: int, movie_id: int, db: Session = Depends(get_db)):
+#     user = db.query(User).filter(User.id == user_id).first()
+#     if not user:
+#         raise HTTPException(status_code=404, detail="User not found")
+
+#     if movie_id not in user.favorites:
+#         raise HTTPException(status_code=400, detail="Movie not in favourites")
+
+#     user.favorites.remove(movie_id)
+#     db.commit()
+#     db.refresh(user)
+
+#     return {"message": "Movie removed from favourites", "favourites": user.favorites}
 
 if __name__ == "__main__":
     import uvicorn
